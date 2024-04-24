9e8c878d960d4773296a7d35b59bf8cec5e6fdc4
Gplus808 <gplus808@gmail.com>
2024-04-24 13:47:10 +0100

    sphere particles added

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -19,14 +19,19 @@ const scene = new THREE.Scene()
  */
 const textureLoader = new THREE.TextureLoader()
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+//Particles
+const particlesGeometry = new THREE.SphereGeometry(1, 32, 32)
+
+//Material
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.02,
+    sizeAttenuation: true
+})
+
+//Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
 
 /**
  * Sizes
